f58b5d0f927ae7ea26ff5558e7120277e7d83be3
brouksafaa <safaabrouk@gmail.com>
2021-07-16 16:43:02 +0100

    backup !!

--- config/db.js
+++ config/db.js
@@ -1,19 +1,13 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const {GridFsStorage} = require('multer-gridfs-storage');
 
 function connectDB() {
     // Database connection 🥳
     mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
     const connection = mongoose.connection;
-    let gfs;
     connection.once('open', () => {
         console.log('Database connected 🥳🥳🥳🥳');
-        gfs = new mongoose.mongo.GridFSBucket(connection.db, {
-            bucketName: "files"
-          });
-    
     }).catch(err => {
         console.log('Connection failed ☹️☹️☹️☹️');
     });
